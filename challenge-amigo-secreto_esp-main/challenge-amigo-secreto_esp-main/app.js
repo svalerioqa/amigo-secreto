00d9338e66ebd290f4ef1017bd6fe959970dfcf3
Simon <svalerioqa@gmail.com>
2025-08-24 19:50:01 -0500

    Ingreso de nombre y lista

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -1,2 +1,11 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
+
+function agregarAmigo() {
+    let nombres = document.getElementById("agregarAmigo");
+    if(nombreIngresado === " "){
+        alert("Por favor, ingrese un nombre");
+        return;
+    }
+}
+amigos.push(agregarAmigo);
