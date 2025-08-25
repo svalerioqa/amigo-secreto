8d9242d3f088a1975ba38449fce14c73cd2d4c5a
Simon <svalerioqa@gmail.com>
2025-08-24 22:36:30 -0500

    Correccion de la funcion agregar amigos

--- challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/challenge-amigo-secreto_esp-main/app.js
@@ -1,11 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 let amigos = []
 
-function agregarAmigo() {
+function agregarAmigo() { 
     let nombres = document.getElementById("agregarAmigo");
-    if(nombreIngresado === " "){
+    let nombreIngresado = nombres.value;
+
+    if(nombreIngresado == "") {
         alert("Por favor, ingrese un nombre");
         return;
     }
+
+amigos.push(nombreIngresado);
+console.log(amigos);
+nombres.value = "";
+
 }
-amigos.push(agregarAmigo);
